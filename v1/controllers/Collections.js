b81f0b55170039251f7644120ece3cbc48e1ac08
Shehryar <shehryar.nadeem.iqbal2@gmail.com>
2022-01-17 20:20:20 +0500

    url fixes

--- v1/controllers/Collections.js
+++ v1/controllers/Collections.js
@@ -6,132 +6,134 @@ const mime = require("mime-types");
 const controller = {};
 
 controller.GetToken = async function (req, res) {
-    const tokenID = req.params.tokenID;
-    try {
-        if (!tokenID) {
-            return res.status(400).send({
-                success: false,
-                error: "Token id is a required parameter"
-            });
-        }
-
-        const token = await NFTTokenModel.findOne({tokenId: tokenID});
-        if (token) {
-            return res.status(200).send({
-                success: true,
-                message: "Token retrieved successfully",
-                data: token
-            });
-        }
-    } catch (ex) {
-        console.log(ex);
-        return res.status(500).send({
-            success: false,
-            error: "server internal error",
-            message: ex
-        });
+  const tokenID = req.params.tokenID;
+  try {
+    if (!tokenID) {
+      return res.status(400).send({
+        success: false,
+        error: "Token id is a required parameter"
+      });
     }
+
+    const token = await NFTTokenModel.findOne({ tokenId: tokenID });
+    if (token) {
+      return res.status(200).send({
+        success: true,
+        message: "Token retrieved successfully",
+        data: token
+      });
+    }
+  } catch (ex) {
+    console.log(ex);
+    return res.status(500).send({
+      success: false,
+      error: "server internal error",
+      message: ex
+    });
+  }
 };
 
 controller.GetArt = async function (req, res) {
-    const artID = req.params.artID;
-    try {
-        if (!artID) {
-            return res.status(400).send({
-                success: false,
-                error: "Art id is a required parameter"
-            });
-        }
-
-        let filePath = path.resolve(__dirname, `../digital-assets/${artID}`);
-        let stat = fileSystem.statSync(filePath);
-
-        res.writeHead(200, {
-            "Content-Type": mime.lookup(filePath),
-            "Content-Length": stat.size
-        });
-
-        let readStream = fileSystem.createReadStream(filePath);
-        readStream.pipe(res);
-    } catch (ex) {
-        console.log(ex);
-        return res.status(500).send({
-            success: false,
-            error: "server internal error",
-            message: ex
-        });
+  const artID = req.params.artID;
+  try {
+    if (!artID) {
+      return res.status(400).send({
+        success: false,
+        error: "Art id is a required parameter"
+      });
     }
+
+    let filePath = path.resolve(__dirname, `../digital-assets/${artID}`);
+    let stat = fileSystem.statSync(filePath);
+
+    res.writeHead(200, {
+      "Content-Type": mime.lookup(filePath),
+      "Content-Length": stat.size
+    });
+
+    let readStream = fileSystem.createReadStream(filePath);
+    readStream.pipe(res);
+  } catch (ex) {
+    console.log(ex);
+    return res.status(500).send({
+      success: false,
+      error: "server internal error",
+      message: ex
+    });
+  }
 };
 
 controller.createCollection = async function (req, res) {
-    try {
-        const thumbnail_image = `${req.protocol}://${req.get("host")}/${req.files["thumbnail_image"][0].path.replace(/\\/g, "/")}`;
-        const timeline_image = `${req.protocol}://${req.get("host")}/${req.files["timeline_image"][0].path.replace(/\\/g, "/")}`;
+  try {
+    const thumbnail_image = `${req.protocol}://${req.get("host")}/${req.files["thumbnail_image"][0].path.replace(/\\/g, "/")}`;
+    const timeline_image = `${req.protocol}://${req.get("host")}/${req.files["timeline_image"][0].path.replace(/\\/g, "/")}`;
 
-        let data = {
-            name: req.body.collection_name,
-            thumbnail_image: thumbnail_image,
-            timeline_image: timeline_image,
-            user: req.user._id
-        };
+    let data = {
+      name: req.body.collection_name,
+      thumbnail_image: thumbnail_image,
+      timeline_image: timeline_image,
+      user: req.user._id
+    };
 
-        const exist = await CollectionModel.find({user: req.user._id, name: data.name});
-        if (exist.length > 0) {
-            return res.status(400).json({success: false, message: "Collection already exist"});
-        }
-
-        const collection = new CollectionModel(data);
-        await collection.save();
-
-        return res.status(200).json({
-            success: true,
-            message: "Collection saved successfully",
-            data: collection
-        });
-    } catch (ex) {
-        return res.status(502).json({
-            success: false,
-            error: ex.message
-        });
+    const exist = await CollectionModel.find({ user: req.user._id, name: data.name });
+    if (exist.length > 0) {
+      return res.status(400).json({ success: false, message: "Collection already exist" });
     }
+
+    const collection = new CollectionModel(data);
+    await collection.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Collection saved successfully",
+      data: collection
+    });
+  } catch (ex) {
+    return res.status(502).json({
+      success: false,
+      error: ex.message
+    });
+  }
 };
 
 controller.updateCollection = async function (req, res) {
-    try {
-        let data = {
-            name: req.body.collection_name,
-            thumbnail_image: req.files["thumbnail_image"][0].path,
-            timeline_image: req.files["timeline_image"][0].path
-        };
-        const model = await CollectionModel.findByIdAndUpdate(req.params.id, data, {new: true});
+  try {
+    const thumbnail_image = `${req.protocol}://${req.get("host")}/${req.files["thumbnail_image"][0].path.replace(/\\/g, "/")}`;
+    const timeline_image = `${req.protocol}://${req.get("host")}/${req.files["timeline_image"][0].path.replace(/\\/g, "/")}`;
+    let data = {
+      name: req.body.collection_name,
+      thumbnail_image: thumbnail_image,
+      timeline_image: timeline_image
+    };
+    const model = await CollectionModel.findByIdAndUpdate(req.params.id, data, { new: true, useFindAndModify: false });
 
-        return res.status(200).json({
-            success: true,
-            message: "Collection saved successfully",
-            data: model
-        });
-    } catch (ex) {
-        return res.status(502).json({
-            success: false,
-            error: ex.message
-        });
-    }
+    return res.status(200).json({
+      success: true,
+      message: "Collection saved successfully",
+      data: model
+    });
+  } catch (ex) {
+    return res.status(502).json({
+      success: false,
+      error: ex.message
+    });
+  }
 };
 
 controller.GetCollectionsByUser = async function (req, res) {
-    try {
-        let collections = await CollectionModel.find({user: req.params.id});
+  try {
+    let collections = await CollectionModel.find({ user: req.params.id });
 
-        return res.status(200).json({
-            success: true,
-            data: collections
-        });
-    } catch (ex) {
-        return res.status(502).json({
-            success: false,
-            error: ex.message
-        });
-    }
+    return res.status(200).json({
+      success: true,
+      data: collections
+    });
+  } catch (ex) {
+    return res.status(502).json({
+      success: false,
+      error: ex.message
+    });
+  }
 };
 
 module.exports = controller;
--- v1/controllers/NFTToken.js
+++ v1/controllers/NFTToken.js
@@ -7,116 +7,116 @@ const settings = require(`../../server-settings`);
 const controller = {};
 
 controller.GetToken = async function (req, res) {
-    const tokenID = req.params.tokenID;
-    try {
-        if (!tokenID) {
-            return res.status(400).send({
-                success: false,
-                error: "Token id is a required parameter"
-            });
-        }
-
-        const token = await NFTTokenModel.findOne({tokenId: tokenID});
-        if (token) {
-            return res.status(200).send({
-                success: false,
-                message: "Token retrieved successfully",
-                data: token
-            });
-        }
-    } catch (ex) {
-        console.log(ex);
-        return res.status(500).send({
-            success: false,
-            error: "server internal error",
-            message: ex
-        });
+  const tokenID = req.params.tokenID;
+  try {
+    if (!tokenID) {
+      return res.status(400).send({
+        success: false,
+        error: "Token id is a required parameter"
+      });
     }
+
+    const token = await NFTTokenModel.findOne({ tokenId: tokenID });
+    if (token) {
+      return res.status(200).send({
+        success: false,
+        message: "Token retrieved successfully",
+        data: token
+      });
+    }
+  } catch (ex) {
+    console.log(ex);
+    return res.status(500).send({
+      success: false,
+      error: "server internal error",
+      message: ex
+    });
+  }
 };
 
 controller.GetArt = async function (req, res) {
-    const artID = req.params.artID;
-    try {
-        if (!artID) {
-            return res.status(400).send({
-                success: false,
-                error: "Art id is a required parameter"
-            });
-        }
-
-        let filePath = path.resolve(__dirname, `../digital-assets/${artID}`);
-        let stat = fileSystem.statSync(filePath);
-
-        res.writeHead(200, {
-            "Content-Type": mime.lookup(filePath),
-            "Content-Length": stat.size
-        });
-
-        let readStream = fileSystem.createReadStream(filePath);
-        readStream.pipe(res);
-    } catch (ex) {
-        console.log(ex);
-        return res.status(500).send({
-            success: false,
-            error: "server internal error",
-            message: ex
-        });
+  const artID = req.params.artID;
+  try {
+    if (!artID) {
+      return res.status(400).send({
+        success: false,
+        error: "Art id is a required parameter"
+      });
     }
+
+    let filePath = path.resolve(__dirname, `../digital-assets/${artID}`);
+    let stat = fileSystem.statSync(filePath);
+
+    res.writeHead(200, {
+      "Content-Type": mime.lookup(filePath),
+      "Content-Length": stat.size
+    });
+
+    let readStream = fileSystem.createReadStream(filePath);
+    readStream.pipe(res);
+  } catch (ex) {
+    console.log(ex);
+    return res.status(500).send({
+      success: false,
+      error: "server internal error",
+      message: ex
+    });
+  }
 };
 
 controller.GetUserNFTTokens = async function (req, res) {
-    try {
-        const tokens = await NFTTokenModel.find({user: req.user._id});
+  try {
+    const tokens = await NFTTokenModel.find({ user: req.user._id });
 
-        return res.status(200).json({
-            success: true,
-            message: "Token retrieved successfully",
-            data: tokens
-        });
-    } catch (ex) {
-        return res.status(502).json({
-            success: false,
-            message: ex.message
-        });
-    }
+    return res.status(200).json({
+      success: true,
+      message: "Token retrieved successfully",
+      data: tokens
+    });
+  } catch (ex) {
+    return res.status(502).json({
+      success: false,
+      message: ex.message
+    });
+  }
 };
 
 controller.createToken = async function (req, res) {
-    try {
-        const {path} = req.file;
-        const {name, description, tags, collection_id} = req.body;
-        if (!name || !description || !collection_id) {
-            return res.status(400).json({success: false, message: "Name, Description & Collection Id is required"});
-        }
-
-        let data = {
-            name,
-            description,
-            tags,
-            collection_id,
-            image: `${settings.server.clientURL}/${path.replace(/\\/g, "/")}`,
-            user: req.user._id
-        };
-
-        const exist = await NFTTokenModel.find({name: data.name, user: data.user})
-        if (exist.length > 0) {
-            return res.status(400).json({success: false, message: "NFT with same name already exist"});
-        }
-
-        const model = new NFTTokenModel(data);
-        await model.save();
-
-        return res.status(200).json({
-            success: true,
-            message: "Token saved successfully",
-            data: model
-        });
-    } catch (ex) {
-        return res.status(500).json({
-            success: false,
-            error: ex.message
-        });
+  try {
+    const { path } = req.file;
+    const { name, description, tags, collection_id } = req.body;
+    if (!name || !description || !collection_id) {
+      return res.status(400).json({ success: false, message: "Name, Description & Collection Id is required" });
     }
+
+    let data = {
+      name,
+      description,
+      tags,
+      collection_id,
+      image: `${req.protocol}://${req.get("host")}/${path.replace(/\\/g, "/")}`,
+      user: req.user._id
+    };
+
+    const exist = await NFTTokenModel.find({ name: data.name, user: data.user });
+    if (exist.length > 0) {
+      return res.status(400).json({ success: false, message: "NFT with same name already exist" });
+    }
+
+    const model = new NFTTokenModel(data);
+    await model.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Token saved successfully",
+      data: model
+    });
+  } catch (ex) {
+    return res.status(500).json({
+      success: false,
+      error: ex.message
+    });
+  }
 };
 
 module.exports = controller;
