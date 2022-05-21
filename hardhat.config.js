057d91babd2089e411a090fe68add6d956ec9c7c
Shehryar nadeem Iqbal <shehryar.nadeem.iqbal2@gmail.com>
2022-05-21 08:01:59 -0700

    contract new

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,7 +18,7 @@ module.exports = {
         },
     },
     solidity: {
-        version: "0.8.7",
+        version: "0.8.9",
         settings: {
             optimizer: {
                 enabled: true,
