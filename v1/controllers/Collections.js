0539b9d935a75fef0e09b5107b722d32d00a18f9
Shehryar Nadeem Iqbal <shehryar.nadeem.qbal2@gmail.com>
2022-06-22 16:52:26 +0500

    Small fix

--- v1/controllers/Collections.js
+++ v1/controllers/Collections.js
@@ -13,7 +13,7 @@ controller.GetAll = async function (req, res) {
       return res.status(404).json({success: false, message: "User object not found"});
     }
 
-    let collections = await CollectionModel.find({created_by: req.user._id}).populate("Category").exec();
+    let collections = await CollectionModel.find({created_by: req.user._id}).populate("category").exec();
     return res.status(200).json({
       success: true,
       message: "Collections retrieved successfully",
