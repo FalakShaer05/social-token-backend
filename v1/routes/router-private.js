8ea1a92f2a283617dd2d1c532517fb2e7b00b490
Shehryar Nadeem Iqbal <shehryar.nadeem.qbal2@gmail.com>
2022-07-07 13:21:36 +0500

    small fix

--- v1/routes/router-private.js
+++ v1/routes/router-private.js
@@ -30,7 +30,7 @@ module.exports = function RouterPrivate(database, settings) {
   router.post("/nft", upload.single("mintable_art"), NFTToken.Create);
 
   // Mint
-  router.post("/transaction", NFTToken.Create);
+  router.post("/transaction/:id", NFTToken.Create);
 
   // Profile
   router.get("/my-nfts", NFTToken.GetMyAllNFTs);
